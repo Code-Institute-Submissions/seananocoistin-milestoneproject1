6ca0da2ed864488a2400ec0e3d70c95ffd81df34
Seanán Ó Coistín <seananoc@gmail.com>
2020-06-24 16:00:42 +0000

    Blasted Javascript file won't work so I had to repaste the JavaScript code into each html file

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,8 +1,5 @@
-<script>
-//Get the button
-var mybutton = document.getElementById("myBtn");
+mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -13,9 +10,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-</script>
